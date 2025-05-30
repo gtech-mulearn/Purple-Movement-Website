e860ff4b7eb9446b12b398ae10d75cad2cd92881
aravinnndddd <aromalaravind0@gmail.com>
2025-05-30 21:28:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,8 @@ function App() {
           <Manifesto />
           <CountDownTimer endDate={endDate} />
         </div>
-          <Aim/>
+         {/*  
+          <Aim/>*/}
       </div>
    
     </>
