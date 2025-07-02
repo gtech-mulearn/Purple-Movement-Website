d7297769fda304389febdb26179cabe5e5227ca4
Aravind <aromalaravind0@gmail.com>
2025-07-02 14:06:31 +0530

    using iframe temporary

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => (
   <iframe
     src="https://purplemovement-landing-rose.vercel.app/"
-    style={{ width: "100%", height: "100%" }}
+    style={{ width: "100%", height: "100vh" }}
   ></iframe>
 );
 export default App;
